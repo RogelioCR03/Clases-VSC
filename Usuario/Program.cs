87107b9d2e6738f9cafbf57e9039ff8951568758
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-02-24 12:26:49 -0800

    Nueva actividad

---
+++ Usuario/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Usuario
+{
+    class CorreoE
+    {
+        private string correo;
+        public CorreoE(string c)
+        {
+            correo=c;
+        }
+        public string getCorreo()
+        {
+            return correo;
+        }
+    }
+
+    class Usuario
+    {
+        private string nombre;
+        private List <CorreoE> correos;
+        public Usuario (string nombre)
+        {
+            this.nombre=nombre;
+            correos = new List <CorreoE>();
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+        public void AgregaCorreo(CorreoE c)
+        {
+            correos.Add(c);
+        }
+        public void Print()
+        {
+            Console.WriteLine(nombre);
+            foreach (CorreoE c in correos)
+            {
+                Console.WriteLine(c.getCorreo ());
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario u1 = new Usuario("Juan");
+            Console.WriteLine (u1.getNombre());
+            u1.AgregaCorreo(new CorreoE ("juan@a.com"));
+            u1.AgregaCorreo(new CorreoE ("juan@gmail.com"));
+            u1.Print();
+        }
+    }
+}
