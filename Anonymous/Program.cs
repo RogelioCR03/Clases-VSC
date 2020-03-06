8576f70d5245b7de8ec2d25e8dd243c59f6ccaa0
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-03-05 17:30:00 -0800

    Nueva actividad

---
+++ Anonymous/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Anonymous
+{
+    class Persona 
+    {
+        public string nombre, apellido;
+        public Persona(string n, string a)
+        {
+            nombre = n ; apellido = a;
+        }
+
+    class Anonimo
+    {
+        static public void anonimiza(Persona p)
+        {
+            p.nombre="XXXXX";
+            p.apellido="XXXXX";
+        }
+    }   
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona espia = new Persona("Fulano", "Detal");
+            Console.WriteLine(espia.nombre);
+            Anonimo.anonimiza(espia);
+            Console.WriteLine(espia.nombre);
+        }
+    }
+}
