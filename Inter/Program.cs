1d268a4d46c43aa058dea02cc9abc8186d9c5e82
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-04-21 12:08:30 -0700

    .

---
+++ Inter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Inter
+{
+    interface IActor
+    {
+        void actua();
+        void ensayar();
+        int getPremios();
+    }
+
+    interface INacionalidad
+    {
+        string Pais();
+        
+    }
+    abstract class Musico 
+    {
+        protected string _nombre;
+
+        public Musico(string n)
+        {
+            _nombre = n;
+        }
+
+
+        public string Nombre 
+        { 
+        get => _nombre; 
+        set => _nombre = value; 
+        }
+
+        public abstract void afina();
+    }
+
+    class Bajista : Musico, IActor 
+    {
+        public Bajista(string n):base(n)
+        {
+
+        }
+
+        public void actua()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void afina()
+        {
+            Console.WriteLine("{0} afinando su bajo", Nombre);
+        }
+
+        public void ensayar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getPremios()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Interfaz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaz
+{
+    class Usuario : IComparable
+    {
+        public string nombre {get; set;}
+        private string _correo;
+        public Usuario (string n, string c)
+        {
+            nombre = n;
+            correo = c;
+        }
+
+        public string correo {
+            get => _correo;
+            set => _correo = value;
+            }
+
+        public int CompareTo(object obj)
+        {
+            return nombre.CompareTo(((Usuario)obj).nombre);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            usuarios.Add(new Usuario("pam","pam@gmail.com"));
+            usuarios.Add(new Usuario("tom","tom@gmail.com"));
+            usuarios.Add(new Usuario("jim","jim@gmail.com"));
+            usuarios.Add(new Usuario("ana","ana@gmail.com"));
+
+            usuarios.Sort();
+
+            foreach(Usuario u in usuarios)
+            Console.WriteLine(u.nombre);
+        
+        }
+    }
+}
