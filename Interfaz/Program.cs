bada93ecd30e4c4b891e9d523f4f84dfee90ad3d
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-06-06 12:46:25 -0700

    Update Program.cs

--- Interfaz/Program.cs
+++ Interfaz/Program.cs
@@ -17,26 +17,29 @@ namespace Interfaz
             get => _correo;
             set => _correo = value;
             }
-
         public int CompareTo(object obj)
         {
-            return nombre.CompareTo(((Usuario)obj).nombre);
+            return correo.CompareTo(((Usuario)obj).correo);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            int instruccion;
+            Console.WriteLine("Ingrese Instruccion");
+            instruccion = Int32.Parse(Console.ReadLine());
+
             List<Usuario> usuarios = new List<Usuario>();
             usuarios.Add(new Usuario("pam","pam@gmail.com"));
             usuarios.Add(new Usuario("tom","tom@gmail.com"));
             usuarios.Add(new Usuario("jim","jim@gmail.com"));
-            usuarios.Add(new Usuario("ana","ana@gmail.com"));
+            usuarios.Add(new Usuario("ana","mna@gmail.com"));
 
             usuarios.Sort();
-
+            
             foreach(Usuario u in usuarios)
-            Console.WriteLine(u.nombre);
+            Console.WriteLine(u.correo);
         
         }
     }
